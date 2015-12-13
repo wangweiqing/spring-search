b2981f4a692695871401d829c3b97152410a7fb2
王微卿 <wangweiqing@meituan.com>
2015-12-13 17:25:50 +0800

    project init

---
+++ spring-search-web/src/main/java/com/wwq/springsearch/controller/DeamController.java
@@ -0,0 +1,13 @@
+package com.wwq.springsearch.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author wangweiqing
+ * @version 1.0
+ * @Description:
+ * @Date 15/12/13
+ */
+@RestController
+public class DeamController {
+}
